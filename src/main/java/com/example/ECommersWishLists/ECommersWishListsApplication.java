16c44684d75c172c96b9d92594b8617f80f9350b
Tejas Gulhane <tgulhane28@gmail.com>
2024-02-13 09:55:00 +0530

    Initialize project

---
+++ src/main/java/com/example/ECommersWishLists/ECommersWishListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ECommersWishLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommersWishListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommersWishListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ECommersWishLists/ECommersWishListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECommersWishLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommersWishListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
